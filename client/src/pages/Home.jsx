bffec784d85f8265a55b8d25bc6c275e0ba78556
ListyaDwiSubagya <listyodwi062@gmail.com>
2025-07-08 22:46:08 +0700

    home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Featured from '../components/Featured'
+import Banner from '../components/Banner'
+import Testimonials from '../components/Testimonials'
+import Newsletter from '../components/Newsletter'
 
 const Home = () => {
   return (
     <>
         <Hero/>
+        <Featured/>
+        <Banner/>
+        <Testimonials/>
+        <Newsletter/>
     </>
   )
 }
