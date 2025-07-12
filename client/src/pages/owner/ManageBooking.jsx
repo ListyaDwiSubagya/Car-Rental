042565b21ad55e267e061c3c0e4b511be7f5bf26
ListyaDwiSubagya <listyodwi062@gmail.com>
2025-07-12 22:52:12 +0700

    admin dashboard

--- client/src/pages/owner/ManageBooking.jsx
+++ client/src/pages/owner/ManageBooking.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyMyBookingsData } from '../../assets/assets'
 
 const ManageBooking = () => {
+
+    const [bookings, setBookings] = useState([])
+
+    const fetchOwnerBooking = async () => {
+        setBookings(dummyMyBookingsData)
+    }
+
+    useEffect(() => {
+        fetchOwnerBooking()
+    },[])
+
   return (
-    <div>ManageBooking</div>
+    <div></div>
   )
 }
 
